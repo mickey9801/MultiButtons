ea5b09b813c5fa1fce87ea95c84adef052f6d61a
Mickey Chan <mickey@comicparty.com>
2019-11-19 00:24:22 +0800

    Update example
    Remove magic number

--- examples/basic_usage/basic_usage.ino
+++ examples/basic_usage/basic_usage.ino
@@ -3,13 +3,13 @@
 const int btnPin = 14;
 
 // Declare voltage ranges for each button
-const int btnCount = 4;
 int voltageRanges[][2] = {
   {3000,3260},
   {2200,2450},
   {1400,1620},
   {600,810}
 };
+int btnCount = sizeof(voltageRanges)/sizeof(voltageRanges[0]);
 // Must declare buttonHandler callback function and used variables 
 // before generate MultiButtons object
 void buttonHandler (MultiButtons *mb, int btnIndex) {
